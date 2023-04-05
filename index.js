674f668b213cff5c94653b3b0966ee98070177e2
Ronald Barasa <barasaogoro@gmail.com>
2023-04-05 17:22:40 +0300

    added form

--- index.js
+++ index.js
@@ -79,13 +79,16 @@ getPhotos();
 const container = document.querySelector('.searchresults');
 const form = document.getElementById('search-form');
 
-async function getSearchPhotos(query, count) {
+async function getSearchPhotos(query) {
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=${7}&client_id=${ACCESS_KEY}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
     const photos = data.results;
     container.innerHTML = ''; // clear the previous search results
+    container.style.display = 'flex';
+    
+
     // create a div for each photo and add it to the container
     photos.forEach(photo => {
       const div = document.createElement('div');
@@ -97,14 +100,73 @@ async function getSearchPhotos(query, count) {
       div.appendChild(img);
       container.appendChild(div);
     });
+
+    // create a cancel button to close the container
+    const cancelButton = document.createElement('span');
+    cancelButton.id = 'cancel-button';
+    const img = document.createElement('img');
+    img.src = "https://img.icons8.com/ios/50/000000/xbox-x.png";
+    img.alt = 'Cancel';
+    img.className='img-fluid'
+    cancelButton.appendChild(img);
+    container.appendChild(cancelButton)
+
+
+    // Event listener
+    cancelButton.addEventListener('click', () => {
+      container.style.display = 'none';
+    });
   } catch (error) {
     console.error(error);
   }
 }
 
-form.addEventListener('submit', event => {
+const contactform = document.querySelector('contactform')
+
+contactform.addEventListener('submit', event => {
     event.preventDefault(); // prevent form submission
     const query = form.elements['search-input'].value;
     const count = 10;
     getSearchPhotos(query, count);
   });
+
+
+
+  form.addEventListener('submit', event => {
+    // Get the form data
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    
+    // Create a new contact object
+    const contact = {
+      name: name,
+      email: email,
+      message: message
+    };
+    
+    // Send the contact object to the server using the Fetch API
+    fetch('', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(contact)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Clear the form fields on successful submission
+      document.getElementById("name").value = '';
+      document.getElementById("email").value = '';
+      document.getElementById("message").value = '';
+      // Show a success message to the user
+      alert('Your message has been sent!');
+    })
+    .catch(error => {
+      console.error('There was a problem submitting the form:', error);
+      alert('There was a problem submitting your message. Please try again later.');
+    });
+  })
+  
