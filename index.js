0d2173a801a33ff9d4f6feb814ec1baf18b68dd5
Ronald Barasa <barasaogoro@gmail.com>
2023-04-05 18:34:55 +0300

    add contuct us form

--- index.js
+++ index.js
@@ -121,9 +121,9 @@ async function getSearchPhotos(query) {
   }
 }
 
-const contactform = document.querySelector('contactform')
 
-contactform.addEventListener('submit', event => {
+
+form.addEventListener('submit', event => {
     event.preventDefault(); // prevent form submission
     const query = form.elements['search-input'].value;
     const count = 10;
@@ -131,9 +131,44 @@ contactform.addEventListener('submit', event => {
   });
 
 
+  
 
-  form.addEventListener('submit', event => {
-    // Get the form data
+  try {
+    function submitcontactform(contact){
+      
+      // Send the contact object to the server using the Fetch API
+      fetch(' http://localhost:3000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        // Clear the form fields on successful submission
+        document.getElementById("name").value = '';
+        document.getElementById("email").value = '';
+        document.getElementById("message").value = '';
+        // Show a success message to the user
+        alert('Your message has been sent!');
+      })
+      .catch(error => {
+        console.error('There was a problem submitting the form:', error);
+        alert('There was a problem submitting your message. Please try again later.');
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+
+  const contactform = document.querySelector('.contactform')
+
+  contactform.addEventListener('submit', ()=> {
+    // Get the contuctus form data
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
@@ -144,29 +179,7 @@ contactform.addEventListener('submit', event => {
       email: email,
       message: message
     };
-    
-    // Send the contact object to the server using the Fetch API
-    fetch('', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(contact)
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      // Clear the form fields on successful submission
-      document.getElementById("name").value = '';
-      document.getElementById("email").value = '';
-      document.getElementById("message").value = '';
-      // Show a success message to the user
-      alert('Your message has been sent!');
-    })
-    .catch(error => {
-      console.error('There was a problem submitting the form:', error);
-      alert('There was a problem submitting your message. Please try again later.');
-    });
+
+    submitcontactform(contact)
   })
   
